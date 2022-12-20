d67b09eb445615c1c6dd5046b6ddc083fadaf737
Crack-way <rupeshsunuwar2019@gmail.com>
2022-12-20 20:20:59 +0545

    Completing the system incometax

--- Incometaxsystem.py
+++ Incometaxsystem.py
@@ -18,7 +18,7 @@ incomePerMonth=[]
 def Staff_Info():
     n=int(input("Please enter the number of staff you wanted to provide data"))
     for i in range(n):
-        input((f"Enter for the staff [{i + 1}]"))
+        print(f"Enter for the staff [{i + 1}]")
         Staff_No.append(input(f"Enter Staff Name [{i+1}]"))
         address.append(input(f"Enter Address [{i+1}] "))
         panNo.append(input(f"Enter Pan No [{i+ 1}]"))
@@ -26,8 +26,8 @@ def Staff_Info():
         fiscalYear.append(input(f"Enter FY[{i + 1 }]"))
         incomePerMonth.append(int(input(f"Enter Staff per month income Rs [{ i + 1}]")))
         display_Static_Info()
-        taxAmount,incomeAfterTax=Calculate_Tax_Of_Staff(status,incomePerMonth[i])
-        display_Staff_Info(Staff_No[i],address[i],panNo[i],status[i],fiscalYear[i],incomePerMonth[i],taxAmount,incomeAfterTax)
+        taxPercent,taxAmount,incomeAfterTax=Calculate_Tax_Of_Staff(status,incomePerMonth[i])
+        display_Staff_Info(Staff_No[i],address[i],panNo[i],status[i],fiscalYear[i],incomePerMonth[i],taxPercent,taxAmount,incomeAfterTax)
 
 def Calculate_Tax_Of_Staff(status,income):
     flag=None
@@ -36,79 +36,92 @@ def Calculate_Tax_Of_Staff(status,income):
     
     elif status=='N':
         flag=0
+    taxPercent=0
     taxAmount=0
     incomeAfterTax=0
     if(flag):
-       taxAmount,incomeAfterTax=Calculate_Tax_Of_Staff_Married(income)
-       return taxAmount,incomeAfterTax
+       taxPercent,taxAmount,incomeAfterTax=Calculate_Tax_Of_Staff_Married(income)
+       return taxPercent,taxAmount,incomeAfterTax
     else:
-       taxAmount,incomeAfterTax=Calculate_Tax_Of_Staff_Unmarried(income)
-       return taxAmount,incomeAfterTax
+       taxPercent,taxAmount,incomeAfterTax=Calculate_Tax_Of_Staff_Unmarried(income)
+       return taxPercent,taxAmount,incomeAfterTax
 
 
 def Calculate_Tax_Of_Staff_Married(income):
+  taxPercent=0
   taxAmount=0
   incomeAfterTax=0
   if(income<=450000):
+    taxPercent=1
     taxAmount=1/ 100 *(income)
     incomeAfterTax= income - taxAmount
   
   elif(income> 450000 and income<=550000):
+    taxPercent=10
     extra=income - 450000
     taxAmount= 10 / 100 * (extra) + 1 / 100 * 450000
     incomeAfterTax= income - taxAmount
 
   elif(income>550000 and income <=750000):
+    taxPercent=20
     extra=income - 550000
     taxAmount=  (10/100 * (100000)) + (1/100 *(450000)) + (20/100 * (extra))
     incomeAfterTax=income - taxAmount
   
   elif(income>750000 and income<=2000000):
+    taxPercent=30
     extra= income - 750000
     taxAmount= (10/100 *(100000)) + (1/100 *(450000))+ (20/100 *(200000)) + (30/100 * (extra))
     incomeAfterTax=income - taxAmount
 
   elif(income>2000000):
+    taxPercent= 36
     extra=income - 2000000
     taxAmount=(10/100 *(100000)) + (1/100 *(450000))+ (20/100 *(200000)) + (30/100 * (1250000)) + (36/100 * (extra))
 
-  return taxAmount, incomeAfterTax
+  return taxPercent,taxAmount, incomeAfterTax
 
 #Finishing later
 def Calculate_Tax_Of_Staff_Unmarried(income):
+  taxPercent=0
   taxAmount=0
   incomeAfterTax=0
   if(income<=400000):
+    taxPercent=1
     taxAmount=1/ 100 *(income)
     incomeAfterTax= income - taxAmount
   
   elif(income> 400000 and income<=500000):
+    taxPercent=10
     extra=income - 400000
     taxAmount= 10 / 100 * (extra) + 1 / 100 * 400000
     incomeAfterTax= income - taxAmount
 
   elif(income>500000 and income <=750000):
+    taxPercent=20
     extra=income - 500000
     taxAmount=  (10/100 * (100000)) + (1/100 *(400000)) + (20/100 * (extra))
     incomeAfterTax=income - taxAmount
   
   elif(income>700000 and income<=2000000):
+    taxPercent=30
     extra= income - 700000
     taxAmount= (10/100 *(100000)) + (1/100 *(400000))+ (20/100 *(200000)) + (30/100 * (extra))
     incomeAfterTax=income - taxAmount
 
   elif(income>2000000):
+    taxPercent=36
     extra=income - 2000000
     taxAmount=(10/100 *(100000)) + (1/100 *(450000))+ (20/100 *(200000)) + (30/100 * (1250000)) + (36/100 * (extra))
 
-  return taxAmount, incomeAfterTax
+  return taxPercent,taxAmount, incomeAfterTax
    
 
-def display_Staff_Info(name,address,panNo,status,fiscalyear,income,taxAmount,incomeAfterTax):
+def display_Staff_Info(name,address,panNo,status,fiscalyear,income,taxPercent,taxAmount,incomeAfterTax):
     print("Staff name:{0}               Address:{1}".format(name,address))
     print("PAN NO: {p}      FY: {q}      Married Status={r}".format(p=panNo,q=fiscalyear,r=status))
     print("Income:"+str(income))
-    print("Staff {a} with PAN {b} fall under + Tax salb.".format(a=name,b=panNo))
+    print("Staff {a} with PAN {b} fall under {c} Tax salb.".format(a=name,b=panNo,c=taxPercent))
     print("{a}  ({b}) to pay the government is [Rs.]= {c}".format(a=name,b=panNo,c=taxAmount))
     #Writing down in the file
     lines=["Name:"+name,"Address:" +address ,"PanNo" + panNo,"Status"+status,"Fiscalyear"+fiscalyear,"Income before tax:"+
